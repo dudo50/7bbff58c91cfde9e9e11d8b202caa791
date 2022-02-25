d6440ca4429dd7d7268a12e20413a3413f17c1e3
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2022-02-25 16:48:59 +0100

    Repaired bug with calling from devices to proxy

--- spustacproxiny.py
+++ spustacproxiny.py
@@ -7,11 +7,8 @@ HOST, PORT = '0.0.0.0', 0
 #Vytvorime si log subor a pridavame do neho zapisy
 logging.basicConfig(format='%(asctime)s   %(message)s',filename='Dennik hovorov.log',level=logging.INFO,datefmt='%H:%M:%S')
 
-#Ziskame si detaily
-meno = socket.gethostname()
-ipadresa = socket.gethostbyname(meno)
-
-#Ziskame si port
+#Ziskame si port a ip adresu
+ipadresa = str(input("Zadaj ip adresu na ktorej chces rozbehnut server\n"))
 PORT = int(input("Zadaj hodnotu na ktorej chces aby si mal port\n"))
 
 #Zapiseme si nami zvolene parametre recordroute a topvia do globalnych premennych v kniznici
@@ -22,6 +19,6 @@ setRecordRoute(recordroute)
 
 #Spustime server
 server = socketserver.UDPServer((HOST, PORT), UDPHandler)
-print("Server uspesne zalozeny")
+print("Server uspesne zalozeny na " + ipadresa+":"+str(PORT))
 server.serve_forever()
 
